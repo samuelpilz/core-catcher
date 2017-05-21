78ccdd7c8dfa01aa070defb19aa33c6cc8d7caba
Jan Kuschel <liquidblock@users.noreply.github.com>
2017-05-21 02:06:04 +0200

    Introduced the Result Type

--- src/Transport.hs
+++ src/Transport.hs
@@ -30,8 +30,9 @@ data Action
     | TwoMoves Move Move
     deriving (Show, Eq, Ord, Read)
 
-data Rollback = Rollback { state :: GameState, error :: Text } | Fatal { error :: Text }
-type Result a = Either Rollback a
+type Error = Text
+data ErrorHandler = Rollback { state :: GameState, error :: Error } | Fatal { error :: Error }
+type Result a = Either ErrorHandler a
 
 applyAction :: Monad m => (a -> Move -> m a) -> a -> Action -> m a
 applyAction f x (OneMove a)    = f x a
@@ -57,42 +58,78 @@ addAction :: Action -> GameState -> GameState
 addAction a s =
     s { actions = a : actions s }
 
-turns :: GameState -> Maybe [PlayerId]
+turns :: GameState -> Result [PlayerId]
 turns state =
   do
-    (_:ls) <- scanState updateTurn initialTurn state
-    sequence $ map (\x -> case x of (h:_) -> Just h; _ -> Nothing) ls
+    ls <- scanState updateTurn initialTurn state :: Result [(PlayerId, [PlayerId])]
+    return $ map fst ls
 
-initialTurn :: Start -> Maybe [PlayerId]
-initialTurn start = case players start of [] -> Nothing; xs -> Just (cycle xs)
-updateTurn :: [PlayerId] -> Action -> Maybe [PlayerId]
-updateTurn (_:xs) _ = Just xs
-updateTurn _ _ = Nothing
+initialTurn :: Start -> Either Error (PlayerId, [PlayerId])
+initialTurn start = case players start of [] -> Left "0 Players"; (x:xs) -> Right (x, cycle (x:xs))
+updateTurn :: (PlayerId, [PlayerId]) -> Action -> Either Error (PlayerId, [PlayerId])
+updateTurn (_,_:(x:xs)) _ = Right (x, x:xs)
+updateTurn _ _ = Left "0 Players"
 
-foldState :: (a -> Action -> Maybe a) -> (Start -> Maybe a) -> GameState -> Maybe a
-foldState update initial (GameState start actions) =
+mapLeft :: (a -> b) -> Either a c -> Either b c
+mapLeft fn (Left a) = Left $ fn a
+mapLeft _ (Right c) = Right c
+
+mapRight :: (a -> b) -> Either c a -> Either c b
+mapRight fn (Right a) = Right $ fn a
+mapRight _ (Left c) = Left c
+
+foldState :: (a -> Action -> Either Error a) -> (Start -> Either Error a) -> GameState -> Result a
+foldState update initial st@(GameState start _) =
   do
-    s <- initial start
-    foldrM (flip update) s actions
+    s <- mapLeft Fatal $ initial start
+    foldrM fn s (unwrap st)
+      where
+        fn (action,state) result = mapLeft (Rollback state) (update result action)
 
-foldStateWithTurn :: (a -> (Action, PlayerId) -> Maybe a) -> (Start -> Maybe a) -> GameState -> Maybe a
-foldStateWithTurn update initial state =
+unwrap :: GameState -> [(Action, GameState)]
+unwrap (GameState _ []) = []
+unwrap s@(GameState _ (x:xs)) = (x, s) : unwrap (s { actions = xs })
+
+foldStateWithTurn :: (a -> (Action, PlayerId) -> Either Error a) -> (Start -> Either Error a) -> GameState -> Result a
+foldStateWithTurn update initial st@(GameState start _) =
   do
-    ts <- turns state
-    s <- initial (start state)
-    foldrM (flip update) s (zip (actions state) ts)
+    ts <- turns st
+    s <- mapLeft Fatal $ initial start
+    foldrM fn s (zip (unwrap st) ts)
+      where
+        fn ((action,state),turn) result = mapLeft (Rollback state) (update result (action, turn))
 
-scanState :: (a -> Action -> Maybe a) -> (Start -> Maybe a) -> GameState -> Maybe [a]
-scanState update initial (GameState start actions) =
-  scanrM update (initial start) actions
+scanState :: (a -> Action -> Either Error a) -> (Start -> Either Error a) -> GameState -> Result [a]
+scanState update initial st@(GameState start actions) =
+  scanrM'' fn (mapLeft Fatal $ initial start) (unwrap st)
+    where
+      fn result (action, state) = mapLeft (Rollback state) (update result action)
 
 printStatesWithTurns :: GameState -> IO ()
-printStatesWithTurns state = fromMaybe (putStrLn "Error while printing States") $ foldStateWithTurn (\y x -> Just $ y >> (print x)) (const $ Just $ return ()) state
+printStatesWithTurns state = case foldStateWithTurn (\y x -> Right $ y >> print x) (const $ Right $ return ()) state of
+  Right io -> io
+  Left err -> putStrLn (Transport.error err)
 
 scanrM :: (MonoFoldable mono, Monad m) => (a -> Element mono -> m a) -> m a -> mono -> m [a]
-scanrM fn z0 =
+scanrM fn z0 ls = do (x, xs) <- _scanrM fn z0 ls; return (x:xs)
+
+scanrM' :: (MonoFoldable mono, Monad m) => (a -> Element mono -> m a) -> m a -> mono -> m [a]
+scanrM' fn z0 =
   let
-    f b ma = do (a:as) <- ma; n <- (fn a b); return (n:a:as)
-    start = do n <- z0; return [n]
+    f b ma = do ls <- ma; a <- a ls; n <- fn a b; return (n:ls)
+    a [] = z0
+    a (a:as) = return a
+    start = return []
+  in
+    foldr f start
+
+scanrM'' :: (MonoFoldable mono, Monad m) => (a -> Element mono -> m a) -> m a -> mono -> m [a]
+scanrM'' fn z0 ls = do (_, xs) <- _scanrM fn z0 ls; return xs
+
+_scanrM :: (MonoFoldable mono, Monad m) => (a -> Element mono -> m a) -> m a -> mono -> m (a, [a])
+_scanrM fn z0 =
+  let
+    f b ma = do (a, as) <- ma; n <- fn a b; return (n, a:as)
+    start = do n <- z0; return (n, [])
   in
     foldr f start
