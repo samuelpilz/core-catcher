80ae5e14ddce25cf4c1a4cd35854df194019abb0
Hannes Siebenhandl <e1327006@student.tuwien.ac.at>
2017-05-29 13:13:23 +0200

    Add basic test and more travis coverage and so on

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+import           ClassyPrelude
+import           Lib
+import           Test.Framework
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = htfMain htf_thisModulesTests
+
+test_mapRightDoubleValue :: IO ()
+test_mapRightDoubleValue =
+    let
+      x = Right 4 :: Either String Int
+      y = Right 8 :: Either String Int
+    in
+      assertEqual y (mapRight (2*) x)
+
+prop_mapRightWithIdFunction :: Either String Int -> Bool
+prop_mapRightWithIdFunction x =
+    x == mapRight id x
