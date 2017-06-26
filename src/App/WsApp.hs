753a646d39a357627637e8e29c93d68450c9f764
Samuel Pilz <e1327391@student.tuwien.ac.at>
2017-06-26 14:36:42 +0200

    Add server config script

--- src/App/WsApp.hs
+++ src/App/WsApp.hs
@@ -14,6 +14,7 @@ import           Network.Protocol
 
 handle :: IsConnection conn => ClientConnection conn -> TVar (ServerState conn) -> Action -> IO ()
 handle client stateVar action = do
+    putStrLn $ tshow action
     (newState, updateResult) <- atomically $ updateGame stateVar action
 
     case updateResult of
