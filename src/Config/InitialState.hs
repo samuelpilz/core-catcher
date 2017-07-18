17c609076c310dabc116472b19fc15f3ae8b962c
Samuel Pilz <e1327391@student.tuwien.ac.at>
2017-07-18 07:56:23 +0200

    Use IsMap in config

--- src/Config/InitialState.hs
+++ src/Config/InitialState.hs
@@ -5,7 +5,6 @@
 module Config.InitialState where
 -- TODO: remove this hard-coded starting-state
 import           ClassyPrelude
-import qualified Data.Map         as Map
 import           Network.Protocol
 
 initialRogueHistory :: RogueHistory
@@ -16,30 +15,25 @@ startPlayer = Player 0
 
 initialPlayerPositions :: PlayerPositions
 initialPlayerPositions =
-    PlayerPositions { playerPositions =
-        Map.fromList
-            [ ( Player { playerId = 0 }, Node { nodeId = 1 } )
-            , ( Player { playerId = 1 }, Node { nodeId = 4 } )
-            , ( Player { playerId = 2 }, Node { nodeId = 2 } )
-            , ( Player { playerId = 3 }, Node { nodeId = 14 } )
-            ]
-    }
+    mapFromList
+        [ ( Player { playerId = 0 }, Node { nodeId = 1 } )
+        , ( Player { playerId = 1 }, Node { nodeId = 4 } )
+        , ( Player { playerId = 2 }, Node { nodeId = 2 } )
+        , ( Player { playerId = 3 }, Node { nodeId = 14 } )
+        ]
 
 
 initialPlayerEnergies :: PlayerEnergies
 initialPlayerEnergies =
-    PlayerEnergies .
-        Map.fromList $
-            map (\playerId -> (Player playerId, initialEnergiesPerPlayer)) [0..3]
+    mapFromList $
+        map (\playerId -> (Player playerId, initialEnergiesPerPlayer)) [0..3]
 
 
 initialEnergiesPerPlayer :: EnergyMap
 initialEnergiesPerPlayer =
-    EnergyMap { energyMap =
-        Map.fromList
-            [ ( Transport { transportName = "orange" }, 5 )
-            , ( Transport { transportName = "blue" }, 3 )
-            , ( Transport { transportName = "red" }, 2 )
-            ]
-    }
+    mapFromList
+        [ ( Transport { transportName = "orange" }, 5 )
+        , ( Transport { transportName = "blue" }, 3 )
+        , ( Transport { transportName = "red" }, 2 )
+        ]
 
