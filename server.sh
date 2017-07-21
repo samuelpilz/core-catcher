1403751a5e774174b99bd19391aa08c0cf872eb5
Samuel Pilz <e1327391@student.tuwien.ac.at>
2017-07-21 18:21:05 +0200

    add static file serving

--- server.sh
+++ server.sh
@@ -3,36 +3,12 @@
 set -e
 set -x
 
-host=$1
-if [[ -z host ]]; then
-  host="localhost"
-fi
-stack_pid=
-elm_pid=
-trap 'set -x; kill $stack_pid; kill $elm_pid; echo; exit' INT
-
 stack build
 stack test
+
 stack exec elm-bridge
 elm-package install --yes
 elm-make frontend/Main.elm --output web/elm.js
 
-stack exec core-catcher-exe &
-stack_pid=$!
-
-elm-live frontend/Main.elm --output web/elm.js --host=$host --dir=web --yes &
-elm_pid=$!
-
-set +x
-
-echo "stack: $stack_pid"
-echo "elm: $elm_pid"
-
-while read p ; do
-  true
-done
-
-kill $stack_pid
-kill $elm_pid
-
+stack exec core-catcher-exe
 
